9c1796d977ca7861f348942ea7064fbafe98fece
Cesar Guajardo <57204124+cesargua@users.noreply.github.com>
2023-08-30 23:27:41 -0700

    Time: 65 ms (67.24%), Space: 44.1 MB (98.58%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,19 +11,17 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    var curr = head;
+    var fast = head;
+    var slow = head;
     var memo = [];
     var pos = 0;
     
-    while(curr){
-        if(memo.find((elem)=>elem===curr) === undefined){
-            console.log('new value:', curr.val)
-            memo.push(curr);
-        } else{
-            return true;
-        }
-        
-        curr = curr.next
+    while(fast && slow && fast.next){
+       fast = fast.next.next
+       slow = slow.next
+       if(fast === slow){
+           return true;
+       }
     }
     return false;
 };
